84aeaf0a5696dcca84868eebe60efe16c77f7d04
msm17b019 <msm17b019@iiitdm.ac.in>
2021-06-15 10:45:25 +0530

    commit.

---
+++ var.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int marks=96;
+    cout<<"My marks in math is "<<marks<<endl;
+    short a;
+    int const b=64;
+    long c;
+    long long d;
+    float score=45.32;
+    double score2=45.344;
+    long double score3=45.332;
+    cout<<"My score is "<<score3<<endl;
+    return 0;
+}
