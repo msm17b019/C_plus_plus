e2656244db9f95c107f20aceeb7696ed44902e2f
msm17b019 <msm17b019@iiitdm.ac.in>
2021-06-15 10:32:45 +0530

    commit.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //This is comment.
+    cout<<"Hiii,"<<endl<<"My name is Sujeet."<<endl;
+    int a,b,c;
+    short sa=9;
+    cout<<sa<<endl;
+    return 0;
+}
